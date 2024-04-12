9dc9dfb7169c4de9889373e62b3aedab11285af6
Martin Chavez <martinchavez1203@gmail.com>
2024-04-11 18:55:20 -0600

    create search plants facade and add helper method #create_plant_poro

--- app/facades/plants_facade.rb
+++ app/facades/plants_facade.rb
@@ -1,12 +1,31 @@
 class PlantsFacade
 
   def self.all_plants
-    plants_json = PlantsService.call_db("/api/v1/plants")
-    plants_json[:data].map { |plant| Plant.new(plant) }
+    plants = PlantsService.call_db("/api/v1/plants")
+  #  plants[:data].map { |plant| Plant.new(plant) }
+   
+   # require 'pry'; binding.pry
+   create_plant_poro(plants)
+  end
+  
+  def self.plant_by_id(id)
+    plant = PlantsService.call_db("/api/v1/plants/#{id}")
+    Plant.new(plant[:data])
+  end
+  
+  def self.search_plants(plant_name)
+    params = { q: plant_name}
+    # require 'pry'; binding.pry
+    plants = PlantsService.call_db("/api/v1/plants/search", params  )
+    # plants[:data].map { |plant| Plant.new(plant) }
+    create_plant_poro(plants)
   end
 
-  def self.plant_by_id(id)
-    plant_json = PlantsService.call_db("/api/v1/plants/#{id}")
-    Plant.new(plant_json[:data])
+  # private 
+
+  def self.create_plant_poro(response)
+   response[:data].map { |plant| Plant.new(plant) }
   end
+
+  #why does this helper method not function
 end
