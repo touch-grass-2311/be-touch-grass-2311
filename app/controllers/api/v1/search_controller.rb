1cbafd728bc0261cfc5ea07682641fbcfc279059
Martin Chavez <martinchavez1203@gmail.com>
2024-04-11 18:57:08 -0600

    create search controller

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,5 +1,9 @@
-class SearchController < ApplicationController
+class Api::V1::SearchController < ApplicationController
   def index 
- #call facade and then setup our endpoint
+    # require 'pry'; binding.pry
+    @plants = PlantsFacade.search_plants(params[:q])
+    render json: PlantSerializer.new(@plants), status: :ok
+
+    #call facade and then setup our endpoint
   end
 end
