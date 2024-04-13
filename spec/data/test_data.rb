e98fbf60db509aef7151e49ff294c08922a79d6f
Martin Chavez <martinchavez1203@gmail.com>
2024-04-12 18:58:35 -0600

    create test for poro index and show

---
+++ spec/data/test_data.rb
@@ -0,0 +1,250 @@
+module TestData 
+  def self.plant_data
+    data = {
+        "id": 77116,
+        "common_name": "Evergreen oak",
+        "slug": "quercus-rotundifolia",
+        "scientific_name": "Quercus rotundifolia",
+        "year": 1785,
+        "bibliography": "Encycl. 1: 723 (1785)",
+        "author": "Lam.",
+        "status": "accepted",
+        "rank": "species",
+        "family_common_name": "null",
+        "genus_id": 3519,
+        "image_url": "https://d2seqvvyy3b8p2.cloudfront.net/40ab8e7cdddbe3e78a581b84efa4e893.jpg",
+        "synonyms": [ "Quercus ilex var. oleoides", "Quercus ilex subvar. rotundifolia", "Quercus ilex f. macrophylla", "Quercus ilex f. oleoides", "Quercus ilex var. calicina", "Quercus ilex subsp. rotundifolia", "Quercus lyauteyi", "Quercus ballota var. rotundifolia", "Quercus ilex f. brevicupulata", "Quercus ilex subvar. major", "Quercus ilex var. pendula", "Quercus rotundifolia f. dolichocalyx", "Quercus calicina", "Quercus rotundifolia f. pilosella", "Quercus rotundifolia f. macrocarpa", "Quercus ilex var. rotundifolia", "Quercus sugaro", "Quercus ilex subvar. pendula", "Quercus ilex f. pendula", "Quercus ilex f. ballota", "Quercus ilex f. rotundifolia", "Quercus ilex subvar. minor", "Quercus ballota", "Quercus ilex var. ballota", "Quercus ilex f. calicina", "Quercus ilex var. microcarpa", "Quercus rotundifolia f. calicina", "Quercus ilex f. macrocarpa", "Quercus rotundifolia f. brevicupulata", "Quercus rotundifolia var. macrocarpa", "Quercus ilex var. brevicupulata", "Quercus ilex subsp. ballota", "Quercus ilex var. dolichocalyx", "Quercus rotundifolia var. pilosella", "Quercus rotundifolia var. brevicupulata", "Quercus rotundifolia subsp. maghrebiana"],
+        "genus": "Quercus",
+        "family": "Fagaceae",
+        "links": {
+            "self": "/api/v1/species/quercus-rotundifolia",
+            "plant": "/api/v1/plants/quercus-rotundifolia",
+            "genus": "/api/v1/genus/quercus"
+        }
+    }
+  end
+
+  def self.plant_show_data 
+    data = {
+      
+          "id": 4589,
+          "common_name": "null",
+          "slug": "centaurea-dimorpha",
+          "scientific_name": "Centaurea dimorpha",
+          "main_species_id": 4589,
+          "image_url": "null",
+          "year": 1824,
+          "bibliography": "(1824). Fl. Libyc. Spec.",
+          "author": "Viv.",
+          "family_common_name": "Aster family",
+          "genus_id": 19,
+          "observations": "null",
+          "vegetable": "null",
+          "links": {
+              "self": "/api/v1/plants/centaurea-dimorpha",
+              "species": "/api/v1/plants/centaurea-dimorpha/species",
+              "genus": "/api/v1/genus/centaurea"
+          },
+          "main_species": {
+              "id": 4589,
+              "common_name": "null",
+              "slug": "centaurea-dimorpha",
+              "scientific_name": "Centaurea dimorpha",
+              "year": 1824,
+              "bibliography": "(1824). Fl. Libyc. Spec.",
+              "author": "Viv.",
+              "status": "accepted",
+              "rank": "species",
+              "family_common_name": "Aster family",
+              "genus_id": 19,
+              "observations": "null",
+              "vegetable": "null",
+              "image_url": "null",
+              "genus": "Centaurea",
+              "family": "Asteraceae",
+              "duration": "null",
+              "edible_part": "null",
+              "edible": "null",
+              "images": {},
+              "common_names": {},
+              "distribution": {},
+              "distributions": {},
+              "flower": {
+                  "color": "null",
+                  "conspicuous": "null"
+              },
+              "foliage": {
+                  "texture": "null",
+                  "color": "null",
+                  "leaf_retention": "null"
+              },
+              "fruit_or_seed": {
+                  "conspicuous": "null",
+                  "color": "null",
+                  "shape": "null",
+                  "seed_persistence": "null"
+              },
+              "sources": [
+                  {
+                      "last_update": "2022-12-04T18:50:59.079Z",
+                      "id": "wfo-0000016146",
+                      "name": "WFO",
+                      "url": "null",
+                      "citation": "null"
+                  },
+                  {
+                      "last_update": "2022-12-04T18:50:59.120Z",
+                      "id": "190374-1",
+                      "name": "IPNI",
+                      "url": "null",
+                      "citation": "null"
+                  },
+                  {
+                      "last_update": "2022-12-07T00:32:48.037Z",
+                      "id": "3127948",
+                      "name": "GBIF",
+                      "url": "https://www.gbif.org/species/3127948",
+                      "citation": "null"
+                  }
+              ],
+              "specifications": {
+                  "ligneous_type": "null",
+                  "growth_form": "null",
+                  "growth_habit": "null",
+                  "growth_rate": "null",
+                  "average_height": {
+                      "cm": "null"
+                  },
+                  "maximum_height": {
+                      "cm": "null"
+                  },
+                  "nitrogen_fixation": "null",
+                  "shape_and_orientation": "null",
+                  "toxicity": "null"
+              },
+              "synonyms": [],
+              "growth": {
+                  "description": "null",
+                  "sowing": "null",
+                  "days_to_harvest": "null",
+                  "row_spacing": {
+                      "cm": "null"
+                  },
+                  "spread": {
+                      "cm": "null"
+                  },
+                  "ph_maximum": "null",
+                  "ph_minimum": "null",
+                  "light": "null",
+                  "atmospheric_humidity": "null",
+                  "growth_months": "null",
+                  "bloom_months": "null",
+                  "fruit_months": "null",
+                  "minimum_precipitation": {
+                      "mm": "null"
+                  },
+                  "maximum_precipitation": {
+                      "mm": "null"
+                  },
+                  "minimum_root_depth": {
+                      "cm": "null"
+                  },
+                  "minimum_temperature": {
+                      "deg_f": "null",
+                      "deg_c": "null"
+                  },
+                  "maximum_temperature": {
+                      "deg_f": "null",
+                      "deg_c": "null"
+                  },
+                  "soil_nutriments": "null",
+                  "soil_salinity": "null",
+                  "soil_texture": "null",
+                  "soil_humidity": "null"
+              },
+              "links": {
+                  "self": "/api/v1/species/centaurea-dimorpha",
+                  "plant": "/api/v1/plants/centaurea-dimorpha",
+                  "genus": "/api/v1/genus/centaurea"
+              }
+          },
+          "genus": {
+              "id": 19,
+              "name": "Centaurea",
+              "slug": "centaurea",
+              "links": {
+                  "self": "/api/v1/genus/centaurea",
+                  "plants": "/api/v1/genus/centaurea/plants",
+                  "species": "/api/v1/genus/centaurea/species",
+                  "family": "/api/v1/families/asteraceae"
+              }
+          },
+          "family": {
+              "id": 13,
+              "name": "Asteraceae",
+              "common_name": "Aster family",
+              "slug": "asteraceae",
+              "links": {
+                  "self": "/api/v1/families/asteraceae",
+                  "division_order": "/api/v1/division_orders/asterales",
+                  "genus": "/api/v1/families/asteraceae/genus"
+              }
+          },
+          "species": [
+              {
+                  "id": 4589,
+                  "common_name": "null",
+                  "slug": "centaurea-dimorpha",
+                  "scientific_name": "Centaurea dimorpha",
+                  "year": 1824,
+                  "bibliography": "(1824). Fl. Libyc. Spec.",
+                  "author": "Viv.",
+                  "status": "accepted",
+                  "rank": "species",
+                  "family_common_name": "Aster family",
+                  "genus_id": 19,
+                  "image_url": "null",
+                  "synonyms": [],
+                  "genus": "Centaurea",
+                  "family": "Asteraceae",
+                  "links": {
+                      "self": "/api/v1/species/centaurea-dimorpha",
+                      "plant": "/api/v1/plants/centaurea-dimorpha",
+                      "genus": "/api/v1/genus/centaurea"
+                  }
+              }
+          ],
+          "subspecies": [],
+          "varieties": [],
+          "hybrids": [],
+          "forms": [],
+          "subvarieties": [],
+          "sources": [
+              {
+                  "last_update": "2022-12-04T18:50:59.079Z",
+                  "id": "wfo-0000016146",
+                  "name": "WFO",
+                  "url": "null",
+                  "citation": "null"
+              },
+              {
+                  "last_update": "2022-12-04T18:50:59.120Z",
+                  "id": "190374-1",
+                  "name": "IPNI",
+                  "url": "null",
+                  "citation": "null"
+              },
+              {
+                  "last_update": "2022-12-07T00:32:48.037Z",
+                  "id": "3127948",
+                  "name": "GBIF",
+                  "url": "https://www.gbif.org/species/3127948",
+                  "citation": "null"
+              }
+          ]
+      }
+  
+  end
+
+  
+end
