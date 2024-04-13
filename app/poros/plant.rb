1c14f75d2d8ad86812a25ffa61f72403661e675f
Martin Chavez <martinchavez1203@gmail.com>
2024-04-12 19:00:03 -0600

    Modified poro and facade to take page

--- app/poros/plant.rb
+++ app/poros/plant.rb
@@ -11,9 +11,11 @@ class Plant
               :ph_min,
               :light,
               :min_precipitation,
-              :data
+              :data,
+              :family
 
   def initialize(data)
+    # require 'pry'; binding.pry
     @data = data
     @id = data[:id]
     @common_name = data[:common_name]
@@ -21,12 +23,13 @@ class Plant
     @family_common_name = data[:family_common_name].to_s
     @scientific_name = data[:scientific_name]
     @image_url = data[:image_url]
-    @edible = data[:edible]
-    @ph_max = extract_data(:ph_max)
+    @edible = data[:edible].to_s
+    @ph_max = extract_data(:ph_maximum)
     @bloom_months = extract_data(:bloom_months)
-    @ph_min = extract_data(:ph_min)
+    @ph_min = extract_data(:ph_minimum)
     @light = extract_data(:light)
-    @min_precipitation = extract_data(:min_precipitation)
+    @min_precipitation = extract_data(:minimum_precipitation)
+    @family = extract_family_data(:family)
 
   end
 
@@ -34,5 +37,9 @@ class Plant
 
   def extract_data(key)
     @data[:main_species]&.dig(:growth, key) || "N/A"  # Set a default value (e.g., "N/A")
+  end
+
+  def extract_family_data(key)
+    @data[:main_species]&.dig(key) || "N/A"
   end
 end
