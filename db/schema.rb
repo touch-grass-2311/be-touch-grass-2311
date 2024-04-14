14bf5f8a0cbcd10f75882d36c0bffb6b58ba4426
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-14 15:55:02 -0600

    Remove 'state' attribute; add 'uid' attribute - a user's Github ID - to act as PK front end is uses to search for current users or save new ones.

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_162558) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "state"
     t.string "plants", default: [], array: true
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
