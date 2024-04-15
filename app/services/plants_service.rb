460a5093dfc25f31db223c84d5e8cd1d8989e2fc
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-15 14:38:00 -0700

    Adjust api_key to read env var

--- app/services/plants_service.rb
+++ app/services/plants_service.rb
@@ -3,7 +3,7 @@ class PlantsService
   def self.call_db(url, params = {})		
     response = connection.get(url) do |request| 
       request.params = params
-      request.params[:token] = Rails.application.credentials.trefle[:key]
+      request.params[:token] = ENV['trefle']
       binding.pry
     end		
     JSON.parse(response.body, symbolize_names: true)		
