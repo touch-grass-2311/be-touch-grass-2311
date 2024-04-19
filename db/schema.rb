4441b348bd98dee576fd60e3525ceca341f487a2
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-18 20:38:07 -0600

    Remove state and add uid to user.  UID very important -> is PK/FK for users

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_063024) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "state"
     t.string "plants", default: [], array: true
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "access_token"
     t.string "avatar_url"
   end
-
 end
