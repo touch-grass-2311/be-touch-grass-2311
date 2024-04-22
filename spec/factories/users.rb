e4544c90107aa6b37312bd04e2eb3e521bb2e580
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-22 01:22:25 -0600

    Add a Faker::alphanumeric tag to create an access_token-like value; needed to register user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     uid { Faker::Bank.account_number(digits: 8) }
+    access_token { Faker::Alphanumeric.alphanumeric(number: 12) }
     # state { "MyString" }
   end
 end
