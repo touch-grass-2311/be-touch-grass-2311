ba9f222090fe8f9bc01c657bdb1d07621a9d0d9c
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-22 01:23:12 -0600

    Remove presence_of validation from user.rb model but kept authenticity validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it {should validate_presence_of(:name)}
-  it {should validate_presence_of(:email)}
-  # it {should validate_uniqueness_of(:name)}
   it {should validate_uniqueness_of(:email)}
   it { should allow_value('user@example.com').for(:email) }
   it { should_not allow_value('invalid_email').for(:email) }
