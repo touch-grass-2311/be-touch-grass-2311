9d4e653da4d728f80d7d1bf0c7a829ff4514231a
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-22 01:20:26 -0600

    Remove duplicate :uid attribute being added to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_04_19_050304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +26,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_19_050304) do
     t.string "name"
     t.string "email"
     t.string "plants", default: [], array: true
-    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "access_token"
