4f90d6c594d2097e9ec1d39cd8c06d11b0d69177
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-22 01:16:56 -0600

    Remove presence validation from :email as not all GH users include email in profiles. Keep uniqueness validation as an email should be unique to one user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-
   validates :uid, presence: true, uniqueness: true
   validates :name, presence: true
-  # validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :access_token, presence: true, uniqueness: true
 
   def update_plants(plant_id)
